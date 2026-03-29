aaa0883d44c9d9dea1a7ed8b8f610c056a9e6d44
Yuvraj Sanap <110754561+YuvrajS99@users.noreply.github.com>
2026-03-29 22:33:07 +0530

    Add import for contactController functions

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Contact = require("../models/Contact");
+
 const {
   validateContactInput,
   sendContactConfirmationEmail
